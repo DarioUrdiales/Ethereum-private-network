87ea23540a4f1b700c0ee17dbb29589180f9720d
DarioUrdiales <dario.r.urdiales@gmail.com>
2023-12-04 20:43:02 +0100

    Eliminamos componente Card innecesario y modificamos rutas

--- front/components/Faucet.jsx
+++ front/components/Faucet.jsx
@@ -1,12 +1,7 @@
-import React, { useEffect, useState } from "react";
-import { Logo } from "./Logo";
-import datos from "../src/datos.json";
+import { useEffect, useState } from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
-import { Parallax } from "react-parallax";
-import { Footer } from "./Footer";
-import { Header } from "./Header";
 
 /**
  * CustomCard component for displaying a styled card.
@@ -117,89 +112,85 @@ export function Faucet() {
   }
 
   return (
-    <Parallax strength={400}>
-      <Header />
-      <div
-        className="bg-black min-vh-100 d-flex flex-column justify-content-center"
-        style={{ background: "#0C290E" }}>
-        {" "}
-        <div className="container my-5">
-          <div className="row justify-content-center">
-            <div className="col-lg-8">
-              {error && (
-                <div
-                  className="alert alert-warning mt-3"
-                  role="alert"
-                  style={{
-                    borderRadius: "5px",
-                    backgroundColor: "#DABB9F",
-                    color: "#333",
-                    maxWidth: "1000px",
-                    margin: "0 auto",
-                  }}>
-                  {error}
-                </div>
-              )}
-
-              {/* Connect Wallet Button (Top Right) */}
-              {!account ? (
-                <Button
-                  onClick={connectWallet}
-                  className="btn btn-primary fs-4 align-self-end mt-2 me-2">
-                  Connect Wallet
-                </Button>
-              ) : null}
-
-              {/* Custom Card components */}
-              <CustomCard
-                title={`Address: ${account || "Not connected"}`}
-                background="#FFFFFF"
-                titleColor="#000000"
-              />
-
-              <CustomCard
-                title={`Balance: ${balance}`}
-                background="#FFFFFF"
-                titleColor="#000000"
-              />
-              <CustomCard
-                title="Receive faucet ERC20 to your wallet"
+    <div
+      className="bg-black min-vh-100 d-flex flex-column justify-content-center"
+      style={{ background: "#0C290E" }}>
+      {" "}
+      <div className="container my-5">
+        <div className="row justify-content-center">
+          <div className="col-lg-8">
+            {error && (
+              <div
+                className="alert alert-warning mt-3"
+                role="alert"
                 style={{
-                  color: "#FFFFFF",
+                  borderRadius: "5px",
+                  backgroundColor: "#DABB9F",
+                  color: "#333",
+                  maxWidth: "1000px",
+                  margin: "0 auto",
                 }}>
-                <div className="d-grid gap-2 mt-10 fs-4">
-                  <Button
-                    onClick={invokeFaucet}
-                    className="btn btn-success mt-4 fs-4">
-                    Get faucet token!
-                  </Button>
-                </div>
-                <div className="d-grid gap-2">
-                  <Button
-                    onClick={fetchBalance}
-                    className="btn btn-secondary mt-3 fs-4">
-                    Check my balance
-                  </Button>
-                </div>
-              </CustomCard>
-              {/* Go Home Button */}
-              <div className="mt-4">
-                <Link
-                  to="/"
-                  className="btn btn-dark fs-4"
-                  style={{
-                    borderRadius: "5px",
-                    maxWidth: "1000px",
-                    margin: "3 auto",
-                  }}>
-                  Go Home
-                </Link>
+                {error}
               </div>
+            )}
+
+            {/* Connect Wallet Button (Top Right) */}
+            {!account ? (
+              <Button
+                onClick={connectWallet}
+                className="btn btn-primary fs-4 align-self-end mt-2 me-2">
+                Connect Wallet
+              </Button>
+            ) : null}
+
+            {/* Custom Card components */}
+            <CustomCard
+              title={`Address: ${account || "Not connected"}`}
+              background="#FFFFFF"
+              titleColor="#000000"
+            />
+
+            <CustomCard
+              title={`Balance: ${balance}`}
+              background="#FFFFFF"
+              titleColor="#000000"
+            />
+            <CustomCard
+              title="Receive faucet ERC20 to your wallet"
+              style={{
+                color: "#FFFFFF",
+              }}>
+              <div className="d-grid gap-2 mt-10 fs-4">
+                <Button
+                  onClick={invokeFaucet}
+                  className="btn btn-success mt-4 fs-4">
+                  Get faucet token!
+                </Button>
+              </div>
+              <div className="d-grid gap-2">
+                <Button
+                  onClick={fetchBalance}
+                  className="btn btn-secondary mt-3 fs-4">
+                  Check my balance
+                </Button>
+              </div>
+            </CustomCard>
+            {/* Go Home Button */}
+            <div className="mt-4">
+              <Link
+                to="/"
+                className="btn btn-dark fs-4"
+                style={{
+                  borderRadius: "5px",
+                  maxWidth: "1000px",
+                  margin: "3 auto",
+                }}>
+                Go Home
+              </Link>
             </div>
           </div>
         </div>
       </div>
-      <Footer />
-    </Parallax>
+    </div>
   );
 }
