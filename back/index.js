3a8b695808f28e876ff363b70271a497affd300c
DarioUrdiales <dario.r.urdiales@gmail.com>
2023-12-07 15:22:07 +0100

    Añadimos scripts para crear las redes automáticamente, listar las redes y borrar una red

--- back/index.js
+++ back/index.js
@@ -20,7 +20,25 @@ const initBlockchain = () => {
   });
 };
 
+const prueba = () => {
+  const command = "cd ../nodos-2 && ./crear_red.sh 3 8000";
+
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error al ejecutar el comando: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.error(`Error en la salida estándar del comando: ${stderr}`);
+      return;
+    }
+
+    console.log(`Salida estándar del comando:\n${stdout}`);
+  });
+}
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
   // initBlockchain();
+  // prueba();
 })
