f874a12a8bd67018f4452894a342166932da4bf2
jackybarraza <jackybarraza@gmail.com>
2023-12-18 10:56:17 +0100

    add component Logo used on header

---
+++ front/src/components/Logo.jsx
@@ -0,0 +1,13 @@
+function Logo({ src, alt }) {
+  return (
+    <img
+      x="0px"
+      y="0px"
+      width="40"
+      height="40"
+      src={src}
+      alt={alt}
+      className="mr-3"
+    />
+  );
+}
